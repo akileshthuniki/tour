1b4965f21a64ba3850c5df208df2bd372fb76f95
akilesh <70111201+akileshthuniki@users.noreply.github.com>
2020-12-06 16:49:55 +0530

    Add files via upload

---
+++ travel.js
@@ -0,0 +1,150 @@
+console.log("hello world");
+/*----------------------------------------------------------------------------*/		
+function myFunction_flight_1() {
+		document.getElementById("myDropdown-flight-1").classList.toggle("show");}
+
+function myFunction_remove_1() {
+		document.getElementById("myDropdown-flight-1").style.display="none";}
+		
+function myFunction_flight_2() {
+		document.getElementById("myDropdown-flight-2").classList.toggle("show");}
+		
+function myFunction_remove_2() {
+		document.getElementById("myDropdown-flight-2").style.display="none";}
+		
+function myFunction_flight_3() {
+		document.getElementById("myDropdown-flight-3").classList.toggle("show");}
+		
+function myFunction_remove_3() {
+		document.getElementById("myDropdown-flight-3").style.display="none";}
+		
+function myFunction_flight_4() {
+		document.getElementById("myDropdown-flight-4").classList.toggle("show");}
+
+function myFunction_remove_4() {
+		document.getElementById("myDropdown-flight-4").style.display="none";}
+		
+function myFunction_flight_5() {
+		document.getElementById("myDropdown-flight-5").classList.toggle("show");}
+
+function myFunction_remove_5() {
+		document.getElementById("myDropdown-flight-5").style.display="none";}
+		
+function myFunction_flight_6() {
+		document.getElementById("myDropdown-flight-6").classList.toggle("show");}
+
+function myFunction_remove_6() {
+		document.getElementById("myDropdown-flight-6").style.display="none";}
+		
+function myFunction_flight_7() {
+		document.getElementById("myDropdown-flight-7").classList.toggle("show");}
+
+function myFunction_remove_7() {
+		document.getElementById("myDropdown-flight-7").style.display="none";}
+		
+function myFunction_flight_8() {
+		document.getElementById("myDropdown-flight-8").classList.toggle("show");}
+
+function myFunction_remove_8() {
+		document.getElementById("myDropdown-flight-8").style.display="none";}
+		
+function myFunction_flight_9() {
+		document.getElementById("myDropdown-flight-9").classList.toggle("show");}
+
+function myFunction_remove_9() {
+		document.getElementById("myDropdown-flight-9").style.display="none";}
+		
+function myFunction_flight_10() {
+		document.getElementById("myDropdown-flight-10").classList.toggle("show");}
+ 
+ function myFunction_remove_10() {
+		document.getElementById("myDropdown-flight-10").style.display="none";}
+								
+/*------------------------------cab-----------------------------------------*/
+function myFunction_cab_11() {
+	document.getElementById("myDropdown-cab-11").classList.toggle("show");}
+
+function cab_remove_11() {
+		document.getElementById("myDropdown-cab-11").style.display="none";}
+	
+function myFunction_cab_12() {
+	document.getElementById("myDropdown-cab-12").classList.toggle("show");}
+
+function cab_remove_12() {
+		document.getElementById("myDropdown-cab-12").style.display="none";}
+
+function myFunction_cab_13() {
+		document.getElementById("myDropdown-cab-13").classList.toggle("show");}
+	
+function cab_remove_13() {
+		document.getElementById("myDropdown-cab-13").style.display="none";}
+		
+function myFunction_cab_14() {
+		document.getElementById("myDropdown-cab-14").classList.toggle("show");}
+		
+function cab_remove_14() {
+		document.getElementById("myDropdown-cab-14").style.display="none";}
+
+/*---------------------------------hotel--------------------------------------*/
+				
+function myFunction_room_1() {
+		document.getElementById("myDropdown-room-1").classList.toggle("show");}
+
+function hotel_remove_1(){
+		document.getElementById("myDropdown-room-1").style.display="none";}
+	
+function myFunction_room_2() {
+		document.getElementById("myDropdown-room-2").classList.toggle("show");}
+
+function hotel_remove_2() {
+		document.getElementById("myDropdown-room-2").style.display="none";}
+		
+function myFunction_room_3() {
+		document.getElementById("myDropdown-room-3").classList.toggle("show");}
+
+function hotel_remove_3() {
+		document.getElementById("myDropdown-room-3").style.display="none";}
+		
+function myFunction_room_4() {
+		document.getElementById("myDropdown-room-4").classList.toggle("show");}
+
+function hotel_remove_4() {
+		document.getElementById("myDropdown-room-4").style.display="none";}
+	
+function myFunction_room_5() {
+		document.getElementById("myDropdown-room-5").classList.toggle("show");}
+
+function hotel_remove_5() {
+		document.getElementById("myDropdown-room-5").style.display="none";}
+		
+function myFunction_room_6() {
+		document.getElementById("myDropdown-room-6").classList.toggle("show");}
+
+function hotel_remove_6() {
+		document.getElementById("myDropdown-room-6").style.display="none";}
+		
+/*--------------------------------holiday-1------------------------------------*/
+		
+function myFunction_holiday_1() {
+		document.getElementById("myDropdown-holiday-1").classList.toggle("show");}
+
+function holiday_remove_1() {
+		document.getElementById("myDropdown-holiday-1").style.display="none";}
+		
+ function myFunction_holiday_2() {
+		document.getElementById("myDropdown-holiday-2").classList.toggle("show");}
+
+function holiday_remove_2() {
+		document.getElementById("myDropdown-holiday-2").style.display="none";}
+		
+ function myFunction_holiday_3() {
+		document.getElementById("myDropdown-holiday-3").classList.toggle("show");}
+
+function holiday_remove_3() {
+		document.getElementById("myDropdown-holiday-3").style.display="none";}
+		
+ function myFunction_holiday_4() {
+		document.getElementById("myDropdown-holiday-4").classList.toggle("show");}
+
+function holiday_remove_4() {
+		document.getElementById("myDropdown-holiday-4").style.display="none";}
